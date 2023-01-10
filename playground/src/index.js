c3e0930d74270d1accffe48e87e15710eba0e88e
Anthony Lisa <alisa@intrivo.com>
2023-01-10 14:50:27 -0800

    Refactor: show greyed out disabled arrows

--- playground/src/index.js
+++ playground/src/index.js
@@ -7,7 +7,15 @@ import * as serviceWorker from './serviceWorker';
 
 const App = () => {
   const [selectedDay, setValue] = useState(null);
-  return <Calendar value={selectedDay} onChange={setValue} shouldHighlightWeekends />;
+  return (
+    <Calendar
+      value={selectedDay}
+      minimumDate={{ year: 2023, month: 1, day: 1 }}
+      maximumDate={{ year: 2023, month: 2, day: 1 }}
+      onChange={setValue}
+      shouldHighlightWeekends
+    />
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
