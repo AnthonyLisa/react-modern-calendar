9d9833e8a66194538cd8b6310771213527307298
Anthony Lisa <alisa@intrivo.com>
2023-01-19 15:47:22 -0800

    fix: use chevrons instead of arrows

--- src/components/Header.js
+++ src/components/Header.js
@@ -176,7 +176,20 @@ const Header = ({
         type="button"
         disabled={isPreviousMonthArrowDisabled}
       >
-        <span className="Calendar__monthArrow" />
+        <span className="Calendar__monthArrow">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="black"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </span>
       </button>
       <div
         className="Calendar__monthYearContainer"
@@ -195,7 +208,20 @@ const Header = ({
         type="button"
         disabled={isNextMonthArrowDisabled}
       >
-        <span className="Calendar__monthArrow" />
+        <span className="Calendar__monthArrow">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="black"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </span>
       </button>
     </div>
   );
