4cafba87c351a41243e7ae695b7dfff7fe8f9742
Anthony Lisa <alisa@intrivo.com>
2023-01-20 12:57:44 -0800

    fix: add types for new props

--- index.d.ts
+++ index.d.ts
@@ -36,6 +36,8 @@ export interface CalendarProps<TValue extends Value> {
   calendarRangeEndClassName?: string;
   renderFooter?: React.FC;
   customDaysClassName?: CustomDayClassNameItem[];
+  disableMonthSelect: boolean;
+  disableYearSelect: boolean;
 }
 
 export function Calendar(props: Optional<CalendarProps<DayValue>, 'value'>): React.ReactElement;
@@ -58,7 +60,9 @@ export interface DatePickerProps<TValue extends Value> extends CalendarProps<TVa
 
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
-declare function DatePicker(props: Optional<DatePickerProps<DayValue>, 'value'>): React.ReactElement;
+declare function DatePicker(
+  props: Optional<DatePickerProps<DayValue>, 'value'>,
+): React.ReactElement;
 declare function DatePicker(props: DatePickerProps<Day[]>): React.ReactElement;
 declare function DatePicker(props: DatePickerProps<DayRange>): React.ReactElement;
 
@@ -66,7 +70,7 @@ type WeekDay = {
   name: string;
   short: string;
   isWeekend?: boolean;
-}
+};
 
 export type Utils = {
   monthsList: string[];
